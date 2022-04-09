4ab8499a54c0d24d1fa4b0a9fb77645873adaf27
VN-OTS-11FQ05N <vonhattruong@Vos-MacBook-Pro.local>
2022-04-09 22:54:51 +0700

    temp

--- array/06.move_zero_of_array.spec.js
+++ array/06.move_zero_of_array.spec.js
@@ -1,38 +1,35 @@
-const {
-  moveAllZeroOfArray,
-  moveAllZeroOfArrayOptimized,
-} = require('./06.move_zero_of_array');
+const { moveAllZeroOfArrayOptimized } = require("./06.move_zero_of_array");
 
-describe('unit tests for moveAllZeroOfArray function', () => {
-  it('moveAllZeroOfArray with case #1', () => {
+describe("unit tests for moveAllZeroOfArray function", () => {
+  it("moveAllZeroOfArray with case #1", () => {
     const result = moveAllZeroOfArrayOptimized([
       1, 10, 20, 0, 59, 63, 0, 88, 0,
     ]);
     expect(result).toEqual([0, 0, 0, 1, 10, 20, 59, 63, 88]);
   });
 
-  it('moveAllZeroOfArray with case #2', () => {
+  it("moveAllZeroOfArray with case #2", () => {
     const result = moveAllZeroOfArrayOptimized([1, 0, 2, 3, 0]);
     expect(result).toEqual([0, 0, 1, 2, 3]);
   });
 
-  it('moveAllZeroOfArray with case #3', () => {
+  it("moveAllZeroOfArray with case #3", () => {
     const result = moveAllZeroOfArrayOptimized([-1, 0, 0, -2, 9]);
     expect(result).toEqual([0, 0, -1, -2, 9]);
   });
 
-  it('moveAllZeroOfArray with case #4', () => {
+  it("moveAllZeroOfArray with case #4", () => {
     const result = moveAllZeroOfArrayOptimized([0]);
     expect(result).toEqual([0]);
   });
 
-  it('moveAllZeroOfArray with case #5', () => {
-    const result = moveAllZeroOfArray([1, 2, 3, 4, 5]);
+  it("moveAllZeroOfArray with case #5", () => {
+    const result = moveAllZeroOfArrayOptimized([1, 2, 3, 4, 5]);
     expect(result).toEqual([1, 2, 3, 4, 5]);
   });
 
-  test('moveAllZeroOfArray with case #6', () => {
-    const result = moveAllZeroOfArray([2]);
+  test("moveAllZeroOfArray with case #6", () => {
+    const result = moveAllZeroOfArrayOptimized([2]);
     expect(result).toEqual([2]);
   });
 });
