53a3cb60a9cf0c81ce92934429eda04e41381177
VN-OTS-11FQ05N <vonhattruong@Vos-MacBook-Pro.local>
2022-04-09 14:19:49 +0700

    optimized fill all zero items in array solutions

--- array/06.move_zero_of_array.js
+++ array/06.move_zero_of_array.js
@@ -2,18 +2,37 @@ const moveAllZeroOfArray = (nums = []) => {
   let totalZero = 0;
   let arrExcludeZeroValue = [];
 
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === 0) {
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 0) {
       totalZero++;
     } else {
-      arrExcludeZeroValue.push(arr[i]);
+      arrExcludeZeroValue.push(nums[i]);
     }
   }
 
   return Array(totalZero).fill(0).concat(arrExcludeZeroValue);
 };
 
-const moveAllZeroOfArrayOptimized = (nums = []) => {};
+const moveAllZeroOfArrayOptimized = (nums = []) => {
+  let readIndex = nums.length - 1;
+  let writeIndex = nums.length - 1;
+
+  while (readIndex >= 0) {
+    if (nums[readIndex] !== 0) {
+      nums[writeIndex] = nums[readIndex];
+      writeIndex--;
+    }
+
+    readIndex--;
+  }
+
+  while (writeIndex >= 0) {
+    nums[writeIndex] = 0;
+    writeIndex--;
+  }
+
+  return nums;
+};
 
 module.exports = {
   moveAllZeroOfArray,
--- array/06.move_zero_of_array.spec.js
+++ array/06.move_zero_of_array.spec.js
@@ -1,23 +1,28 @@
-const { moveAllZeroOfArray } = require('./06.move_zero_of_array');
+const {
+  moveAllZeroOfArray,
+  moveAllZeroOfArrayOptimized,
+} = require('./06.move_zero_of_array');
 
 describe('unit tests for moveAllZeroOfArray function', () => {
   it('moveAllZeroOfArray with case #1', () => {
-    const result = moveAllZeroOfArray([1, 10, 20, 0, 59, 63, 0, 88, 0]);
+    const result = moveAllZeroOfArrayOptimized([
+      1, 10, 20, 0, 59, 63, 0, 88, 0,
+    ]);
     expect(result).toEqual([0, 0, 0, 1, 10, 20, 59, 63, 88]);
   });
 
   it('moveAllZeroOfArray with case #2', () => {
-    const result = moveAllZeroOfArray([1, 0, 2, 3, 0]);
+    const result = moveAllZeroOfArrayOptimized([1, 0, 2, 3, 0]);
     expect(result).toEqual([0, 0, 1, 2, 3]);
   });
 
   it('moveAllZeroOfArray with case #3', () => {
-    const result = moveAllZeroOfArray([-1, 0, 0, -2, 9]);
+    const result = moveAllZeroOfArrayOptimized([-1, 0, 0, -2, 9]);
     expect(result).toEqual([0, 0, -1, -2, 9]);
   });
 
   it('moveAllZeroOfArray with case #4', () => {
-    const result = moveAllZeroOfArray([0]);
+    const result = moveAllZeroOfArrayOptimized([0]);
     expect(result).toEqual([0]);
   });
 
@@ -26,7 +31,7 @@ describe('unit tests for moveAllZeroOfArray function', () => {
     expect(result).toEqual([1, 2, 3, 4, 5]);
   });
 
-  it('moveAllZeroOfArray with case #6', () => {
+  test('moveAllZeroOfArray with case #6', () => {
     const result = moveAllZeroOfArray([2]);
     expect(result).toEqual([2]);
   });
